60b2273f68b48fc5c5cbddc9e0f2f575358da51d
Pablo Ferreira <pablomartinferre@gmail.com>
2023-09-07 11:09:47 -0300

    act

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+## Script para Unix
+flex lexico.l
+bison -dyv sintactico.y
+gcc lex.yy.c y.tab.c -o compilador
+./compilador prueba.txt
+rm lex.yy.c
+rm y.tab.c
+rm y.output
+rm y.tab.h
+rm compilador
