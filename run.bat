0bba2d4a3ef8aac1ecf77ab8aff16c3c18b62d00
Caro-Nunez <carolaranunez@gmail.com>
2023-11-23 17:24:35 -0300

    Cambio librerias de lugar

--- run.bat
+++ run.bat
@@ -1,14 +1,11 @@
-set compilador="lyc-compiler-2.5.0.o"
-set libc="libc/utilidades.c libc/lista_simbolos.c libc/arbol.c libc/pila.c libc/generar_assembler.c"
-
 flex lexico.l
 bison -dyv sintactico.y
-gcc.exe lex.yy.c y.tab.c %libc% -o %compilador%
-./%compilador% casos_de_prueba/test.txt
+gcc.exe lex.yy.c y.tab.c libc/utilidades.c libc/lista_simbolos.c libc/arbol.c libc/pila.c libc/generar_assembler.c -o lyc-compiler-2.5.0.o
+./lyc-compiler-2.5.0.o casos_de_prueba/test.txt
 dot -Tpng intermedia-code.dot -o arbol.png -Gcharset=latin1
 
 @echo off
-del %compilador%
+del lyc-compiler-2.5.0.o
 del lex.yy.c
 del y.tab.c
 del y.tab.h
