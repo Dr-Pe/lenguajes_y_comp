904c488bd4c5a28dbf118da064ee574b1e128edd
Caro-Nunez <carolaranunez@gmail.com>
2023-11-23 19:44:35 -0300

    Arreglo el . de windows

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 flex lexico.l
 bison -dyv sintactico.y
 gcc.exe lex.yy.c y.tab.c libc/utilidades.c libc/lista_simbolos.c libc/arbol.c libc/pila.c libc/generar_assembler.c -o lyc-compiler-2.5.0.o
-./lyc-compiler-2.5.0.o casos_de_prueba/test.txt
+lyc-compiler-2.5.0.o casos_de_prueba/test.txt
 dot -Tpng intermedia-code.dot -o arbol.png -Gcharset=latin1
 
 @echo off
