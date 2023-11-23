381ecffe53d4cced7f43444fd7cd867b9fa7f93b
Caro-Nunez <carolaranunez@gmail.com>
2023-11-23 16:39:44 -0300

    agrego intermedia-code.dot y libreria utilidades.c que no estaba

--- run.bat
+++ run.bat
@@ -1,11 +1,11 @@
 set compilador="lyc-compiler-2.5.0.o"
-set libc="libc/lista_simbolos.c libc/arbol.c libc/pila.c libc/generar_assembler.c"
+set libc="libc/utilidades.c libc/lista_simbolos.c libc/arbol.c libc/pila.c libc/generar_assembler.c"
 
 flex lexico.l
 bison -dyv sintactico.y
 gcc.exe lex.yy.c y.tab.c %libc% -o %compilador%
 ./%compilador% casos_de_prueba/test.txt
-dot -Tpng intermedia.dot -o arbol.png -Gcharset=latin1
+dot -Tpng intermedia-code.dot -o arbol.png -Gcharset=latin1
 
 @echo off
 del %compilador%
